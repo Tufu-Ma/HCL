5958fbe2abfa7d498539c6530c99f97dc6b2b773
oil <tanatip.d@ku.th>
2025-10-07 17:13:17 +0700

    add promotion + edit bg and style about-me , home , add-game

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './shared/home/home.component';
 import { AddGameComponent } from './shared/add-game/add-game.component';
 import { AddGameDetailComponent } from './shared/add-game-detail/add-game-detail.component';
 import { AboutMeComponent } from './shared/about-me/about-me.component';
+import { PromotionComponent } from './shared/promotion/promotion.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch:'full' },
   { path: 'about-me', component: AboutMeComponent },
+  { path: 'promotion', component: PromotionComponent },
   { path: 'addgame', component: AddGameComponent },
   { path: 'addgame-detail/:id', component: AddGameDetailComponent },
 ];
